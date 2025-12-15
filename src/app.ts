a58e9bc6b77fc29e2096a047b7974c25ce0e0e54
eduartrob <eduartrob@gmail.com>
2025-12-15 17:34:50 -0600

    fix: agregar s3Router a app.ts para habilitar endpoints de S3

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { connectDB } from './config/db';
 import userRouter from './routes/userRouter';
 import routineRouter from './routes/routineRouter';
 import progressRouter from './routes/progressRouter';
+import s3Router from './routes/s3Router';
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/routines', routineRouter);
 app.use('/api/progress', progressRouter);
+app.use('/api/s3', s3Router);
 
 connectDB();
 export { app };
